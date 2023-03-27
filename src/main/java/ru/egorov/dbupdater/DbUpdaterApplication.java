97c147855817f429719759f42148a6b4df2b6019
Viktor Egorov <107366919+remowilliamsss@users.noreply.github.com>
2023-03-27 11:03:39 +0300

    initail

---
+++ src/main/java/ru/egorov/dbupdater/DbUpdaterApplication.java
@@ -0,0 +1,13 @@
+package ru.egorov.dbupdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbUpdaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbUpdaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/egorov/dbupdater/DbUpdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.egorov.dbupdater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbUpdaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
